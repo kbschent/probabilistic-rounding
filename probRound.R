9ece881a6b6594bb1be8196912531c31244ce17c
Kyle B. Schenthal <kbschent@gmail.com>
2017-09-26 23:38:54 -0500

    Add comments

--- probRound.R
+++ probRound.R
@@ -4,12 +4,19 @@ probRound = function(...){
     
     # initialize list of rounded numbers
     rounded = vector(mode = "integer", length = length(input)) 
+    
     i <- 1
     
     while (i <= length(input)){
         m <-  input[i] %% 1 # extract the mantissa
-        rounded[i] <- sample(c(floor(input[i]),ceiling(input[i])), size = 1, prob = c(1 - m, m));
+        
+        # sample the floor and ceiling rounding with 
+        # probabilities determined by the mantissa
+        rounded[i] <- sample(c(floor(input[i]),ceiling(input[i])), 
+                             size = 1, prob = c(1 - m, m));
+        
         i = i + 1
     }
+    
     return(rounded)
 }
