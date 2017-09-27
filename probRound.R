f20c98be6635897ebc496f0c178a0fced2b5182c
Kyle B. Schenthal <kbschent@gmail.com>
2017-09-26 23:32:13 -0500

    Add probRound.R

---
+++ probRound.R
@@ -0,0 +1,15 @@
+probRound = function(...){
+    # create vector of arguments
+    input = c(...)
+    
+    # initialize list of rounded numbers
+    rounded = vector(mode = "integer", length = length(input)) 
+    i <- 1
+    
+    while (i <= length(input)){
+        m <-  input[i] %% 1 # extract the mantissa
+        rounded[i] <- sample(c(floor(input[i]),ceiling(input[i])), size = 1, prob = c(1 - m, m));
+        i = i + 1
+    }
+    return(rounded)
+}
